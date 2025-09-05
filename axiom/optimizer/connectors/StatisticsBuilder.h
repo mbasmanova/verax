4021cd2709e5f06c4eac2639ab90d531cd5f73b1
Maria Basmanova <mbasmanova@fb.com>
2025-09-05 16:06:56 -0700

    refactor: Extract StatisticsBuilder into its own header file
    Differential Revision: D81805604

---
+++ axiom/optimizer/connectors/StatisticsBuilder.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "velox/common/memory/HashStringAllocator.h"
+#include "velox/vector/ComplexVector.h"
+
+namespace facebook::velox::connector {
+
+struct ColumnStatistics;
+
+/// Options for StatisticsBuilder.
+struct StatisticsBuilderOptions {
+  int32_t maxStringLength{100};
+  int32_t initialSize{0};
+  bool countDistincts{false};
+  HashStringAllocator* allocator{nullptr};
+};
+
+/// Abstract class for building statistics from samples.
+class StatisticsBuilder {
+ public:
+  virtual ~StatisticsBuilder() = default;
+
+  static std::unique_ptr<StatisticsBuilder> create(
+      const TypePtr& type,
+      const StatisticsBuilderOptions& opts);
+
+  static void updateBuilders(
+      const RowVectorPtr& data,
+      std::vector<std::unique_ptr<StatisticsBuilder>>& builders);
+
+  virtual const TypePtr& type() const = 0;
+
+  /// Accumulates elements of 'vector' into stats.
+  virtual void add(const VectorPtr& data) = 0;
+
+  /// Merges the statistics of 'other' into 'this'.
+  virtual void merge(const StatisticsBuilder& other) = 0;
+
+  /// Fills 'result' with the accumulated stats. Scales up counts by
+  /// 'sampleFraction', e.g. 0.1 means 10x.
+  virtual void build(ColumnStatistics& result, float sampleFraction = 1) = 0;
+
+  virtual int64_t numAscending() const = 0;
+  virtual int64_t numRepeat() const = 0;
+  virtual int64_t numDescending() const = 0;
+};
+
+} // namespace facebook::velox::connector
