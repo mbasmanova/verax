09c24f97dedd02973bea7aad96fbcab9d1e845ef
Masha Basmanova <mbasmanova@meta.com>
2025-09-25 12:36:18 -0700

    refactor: Extract ConnectorSplitManager into its own file (#428)
    Summary: Pull Request resolved: https://github.com/facebookexperimental/verax/pull/428
    
    Reviewed By: pansatadru
    
    Differential Revision: D83241743
    
    fbshipit-source-id: 6ab4b1673396f9c9638c8080a6941f84a13d61ce

--- axiom/connectors/ConnectorMetadata.h
+++ axiom/connectors/ConnectorMetadata.h
@@ -16,6 +16,7 @@
 #pragma once
 
 #include "axiom/common/Enums.h"
+#include "axiom/connectors/ConnectorSplitManager.h"
 #include "velox/common/memory/HashStringAllocator.h"
 #include "velox/connectors/Connector.h"
 #include "velox/type/Subfield.h"
@@ -352,68 +353,6 @@ class Table {
 };
 
 using TablePtr = std::shared_ptr<const Table>;
-
-/// Describes a single partition of a TableLayout. A TableLayout has at least
-/// one partition, even if it has no partitioning columns.
-class PartitionHandle {
- public:
-  virtual ~PartitionHandle() = default;
-};
-
-using PartitionHandlePtr = std::shared_ptr<const PartitionHandle>;
-
-/// Enumerates splits. The table and partitions to cover are given to
-/// ConnectorSplitManager.
-class SplitSource {
- public:
-  static constexpr uint32_t kUngroupedGroupId =
-      std::numeric_limits<uint32_t>::max();
-
-  /// Result of getSplits. Each split belongs to a group. A nullptr split for
-  /// group means that there are no more splits for the group. In ungrouped
-  /// execution, the group is always kUngroupedGroupId.
-  struct SplitAndGroup {
-    std::shared_ptr<velox::connector::ConnectorSplit> split;
-    uint32_t group{kUngroupedGroupId};
-  };
-
-  virtual ~SplitSource() = default;
-
-  /// Returns a set of splits that cover up to 'targetBytes' of data.
-  virtual std::vector<SplitAndGroup> getSplits(uint64_t targetBytes) = 0;
-};
-
-/// Options for split generation.
-struct SplitOptions {
-  /// Make no more than one split per file.
-  bool wholeFile{false};
-
-  /// If non-0, gives a minimum number of splits to generate, e.g. at least one
-  /// for each driver of each worker.
-  int32_t targetSplitCount{0};
-
-  /// Target size of split.
-  uint64_t fileBytesPerSplit{128ULL << 20U};
-};
-
-class ConnectorSplitManager {
- public:
-  virtual ~ConnectorSplitManager() = default;
-
-  /// Returns a list of all partitions that match the filters in
-  /// 'tableHandle'. A non-partitioned table returns one partition.
-  virtual std::vector<PartitionHandlePtr> listPartitions(
-      const velox::connector::ConnectorTableHandlePtr& tableHandle) = 0;
-
-  /// Returns a SplitSource that covers the contents of 'partitions'. The set of
-  /// partitions is exposed separately so that the caller may process the
-  /// partitions in a specific order or distribute them to specific nodes in a
-  /// cluster.
-  virtual std::shared_ptr<SplitSource> getSplitSource(
-      const velox::connector::ConnectorTableHandlePtr& tableHandle,
-      const std::vector<PartitionHandlePtr>& partitions,
-      SplitOptions = {}) = 0;
-};
 
 // TODO Move to velox/type/Subfield.h
 using SubfieldPtr = std::shared_ptr<const velox::common::Subfield>;
