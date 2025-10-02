6cc8850c393919e101992ad3cc615a68bd339e0f
Masha Basmanova <mbasmanova@meta.com>
2025-10-01 23:46:27 -0700

    build: Use gersemi instead of the no longer maintained cmake-format (#457)
    Summary:
    ```
    %  pre-commit run  gersemi --all-files
    CMake formatter..........................................................Passed
    ```
    
    See
    - https://github.com/facebookincubator/velox/blob/904378eb6349c2665f3aa324dd6cd5a13840baed/CODING_STYLE.md?plain=1#L10
    - https://github.com/facebookincubator/velox/pull/14237
    
    Pull Request resolved: https://github.com/facebookincubator/axiom/pull/457
    
    Reviewed By: kgpai
    
    Differential Revision: D83701456
    
    Pulled By: mbasmanova
    
    fbshipit-source-id: 40869b7565a30ec3fd7fcf2e2667fade76e6a946

--- CMake/resolve_dependency_modules/antlr4-runtime.cmake
+++ CMake/resolve_dependency_modules/antlr4-runtime.cmake
@@ -14,10 +14,13 @@
 include_guard(GLOBAL)
 
 set(VELOX_ANTLR4-RUNTIME_VERSION 4.13.2)
-set(VELOX_ANTLR4-RUNTIME_BUILD_SHA256_CHECKSUM
-    0ed13668906e86dbc0dcddf30fdee68c10203dea4e83852b4edb810821bee3c4)
-set(VELOX_ANTLR4-RUNTIME_SOURCE_URL
-    "https://www.antlr.org/download/antlr4-cpp-runtime-${VELOX_ANTLR4-RUNTIME_VERSION}-source.zip"
+set(
+  VELOX_ANTLR4-RUNTIME_BUILD_SHA256_CHECKSUM
+  0ed13668906e86dbc0dcddf30fdee68c10203dea4e83852b4edb810821bee3c4
+)
+set(
+  VELOX_ANTLR4-RUNTIME_SOURCE_URL
+  "https://www.antlr.org/download/antlr4-cpp-runtime-${VELOX_ANTLR4-RUNTIME_VERSION}-source.zip"
 )
 
 velox_resolve_dependency_url(ANTLR4-RUNTIME)
@@ -27,13 +30,10 @@ message(STATUS "Building antlr4-runtime from source")
 FetchContent_Declare(
   antlr4-runtime
   URL ${VELOX_ANTLR4-RUNTIME_SOURCE_URL}
-  URL_HASH ${VELOX_ANTLR4-RUNTIME_BUILD_SHA256_CHECKSUM})
+  URL_HASH ${VELOX_ANTLR4-RUNTIME_BUILD_SHA256_CHECKSUM}
+)
 
-set(ANTLR4_INSTALL
-    OFF
-    CACHE BOOL "Disable installation for antlr4-runtime")
-set(ANTLR_BUILD_CPP_TESTS
-    OFF
-    CACHE BOOL "Disable tests for antlr4-runtime")
+set(ANTLR4_INSTALL OFF CACHE BOOL "Disable installation for antlr4-runtime")
+set(ANTLR_BUILD_CPP_TESTS OFF CACHE BOOL "Disable tests for antlr4-runtime")
 
 FetchContent_MakeAvailable(antlr4-runtime)
